72034feb500b660d3c1af2e78f512edf43700b98
ssalyi <ssalyi@rdisoftware.com>
2017-08-14 16:03:41 +0200

    FC-3-create-entities-config

---
+++ core/src/main/java/com/github/szsalyi/flashcardapp/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.github.szsalyi.flashcardapp.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by ssalyi on 8/14/2017.
+ */
+public class BaseEntity implements Serializable {
+
+    
+}
---
+++ core/src/main/java/com/github/szsalyi/flashcardapp/entity/User.java
@@ -0,0 +1,7 @@
+package com.github.szsalyi.flashcardapp.entity;
+
+/**
+ * Created by ssalyi on 8/14/2017.
+ */
+public class User {
+}
