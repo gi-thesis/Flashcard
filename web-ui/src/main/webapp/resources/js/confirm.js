23a236fa05ab513b4b5fd8a34acac4ea64c10654
ssalyi <ssalyi@rdisoftware.com>
2018-01-05 00:16:44 +0100

    Password confirmation needs to be fixed

--- web-ui/src/main/webapp/resources/js/confirm.js
+++ web-ui/src/main/webapp/resources/js/confirm.js
@@ -1,12 +1,15 @@
 var password = document.getElementById("password")
-var confirm_password = document.getElementById("confirm_password")
+    , confirm_password = document.getElementById("confirm_password");
 
-function validatePassword() {
-    if(confirm_password != password){
-        confirm_password.setCustomValidity("The password don't match")
-    }else
-        confirm_password.setCustomValidity("")
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+        confirm_password.style.backgroundColor = "#F44336";
+        confirm_password.style.border = "#8B0000";
+    } else {
+        confirm_password.setCustomValidity('');
+    }
 }
 
-password.onchange = validatePassword;
-confirm_password.onkeyup = validatePassword;
+password.onchange = validatePassword();
+confirm_password.onkeyup = validatePassword();
