7dc18614286eede84361dfb94dbb87e2f9baf2fd
ssalyi <ssalyi@rdisoftware.com>
2018-01-10 09:46:55 +0100

    FC-20-Registration page is don with validations

--- web-ui/src/main/webapp/resources/js/username-validation.js
+++ web-ui/src/main/webapp/resources/js/username-validation.js
@@ -1,17 +1,18 @@
-var username = $('#user_name');
-var pathname = window.location.pathname;
-var url = window.location.href;
 var data = {
-    'username' : username
+    'username' : $('#user_name')
 }
 $(document).ready(function() {
     $('#user_name').blur(function () {
         $.ajax({
             type: 'GET',
-            url: '/api/users/exists',
+            url: 'api/users/exists',
             dataType: 'json',
-            success: function (data) {
-              console.log('success', data)
+            data: {username : $('#user_name').val()},
+            success: function (response) {
+              if(response.data == true){
+                  console.log('succes',response.data);
+                  $('#user_name').css("border-color", "red");
+              }
             }
         });
     });
