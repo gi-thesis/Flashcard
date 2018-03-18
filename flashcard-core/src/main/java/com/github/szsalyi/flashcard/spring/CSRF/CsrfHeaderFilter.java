0980f25ddaa4f60aecc329dee5cfc7f9443d9a97
ssalyi <ssalyi@rdisoftware.com>
2018-03-18 13:44:53 +0100

    commit-for-review-login

---
+++ flashcard-core/src/main/java/com/github/szsalyi/flashcard/spring/CSRF/CsrfHeaderFilter.java
@@ -0,0 +1,4 @@
+package com.github.szsalyi.flashcard.spring.CSRF;
+
+public class CsrfHeaderFilter {
+}
---
+++ flashcard-core/src/main/java/com/github/szsalyi/flashcard/users/LoginController.java
@@ -0,0 +1,4 @@
+package com.github.szsalyi.flashcard.users;
+
+public class LoginController {
+}
