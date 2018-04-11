438605840aaa82097f809622e774298aab9be5fa
ssalyi <ssalyi@rdisoftware.com>
2018-04-11 11:14:18 +0200

    category-page-css

--- flashcard-ui/src/main/ui/src/app/landing/landing.component.js
+++ flashcard-ui/src/main/ui/src/app/landing/landing.component.js
@@ -1,9 +1,4 @@
 angular.module('fc-app').component('fcLanding', {
     templateUrl: 'app/landing/landing.component.html',
-    controller : function () {
-        var ctrl = this;
-
-
-    },
     controllerAs : 'landingCtrl'
 });
